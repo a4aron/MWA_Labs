2742ca605c8ebc25e65e27ccbafdfe4907cba0a2
a4aron <arun.mgr1@gmail.com>
2018-07-02 15:35:17 -0500

    lab5

--- lab4/part1/childprocess.js
+++ lab4/part1/childprocess.js
@@ -1,24 +1,20 @@
 const fs = require('fs')
-// const path = require('path')
 const readOperation = (filename) =>{
     var folders = filename.split('/');
     var changeUri = (folders.join('\\'))
-    // return __dirname+`\\${changeUri}` ;
-    var l = fs.readFileSync(`./${changeUri}`,{encoding : 'utf8'})
-    console.log(l)
-    return l;
-    // fs.readFile(`./${changeUri}`,(err,content)=>{
+    var data = fs.readFileSync(`./${changeUri}`,{encoding : 'utf8'})
+    return data;
+    // fs.readFile(`./${changeUri}`,{encoding : 'utf8'},(err,content)=>{
     //     if(!err)
-    //          return content
-    //     else return err;
+    //          return content.toString()
+    //     else throw err;
     // });
-    
 }
 
 process.on('message',(filename)=>{
     console.log("in child process")
     var processeddata = readOperation(filename);
-    console.log(processeddata)
+    // console.log(processeddata)
     process.send(processeddata);
 })
 
--- lab4/part1/index.js
+++ lab4/part1/index.js
@@ -1,3 +1,4 @@
+// http://127.0.0.1:8888/?url=path/to/my/file.txt
 const { Subject } = require('rxjs');
 const { fork } = require('child_process');
 const url = require('url')
@@ -7,17 +8,17 @@ function responseHandler(reqresp){
     let req = reqresp.req;
     let res = reqresp.res;
     let urlObj = url.parse((req.url),true);
-    // console.log(urlObj.query.url)
-    const childProcess = fork('childprocess.js')
-    childProcess.send(urlObj.query.url);
-    childProcess.on('message',msg=>{
-        // res.send("response from child")
-        res.writeHead(200,{'content-type':'text/plain'})
-        res.end(msg);
-    })
+    if(urlObj.query.url != "/") {
+        const childProcess = fork('childprocess.js')
+        childProcess.send(urlObj.query.url);
+        childProcess.on('message',msg=>{
+            res.writeHead(200,{'content-type':'text/plain'})
+            res.end(msg);
+        })
+    }
    
 }
-subject.subscribe(responseHandler,(err)=>console.log(err))
+subject.subscribe(responseHandler,(err)=>console.log(err),(err)=>console.log(err))
 const http = require('http');
 http.createServer((req,res)=>{
     subject.next({req: req, res : res})
